1b9bcfadafcaf78f1ea15f6d2e2ee04454c7047c
Tobias-lab351 <todhiambo87@gmail.com>
2025-01-06 13:44:55 +0300

    added a search component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Destination from "./components/destination/Destination";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Destination />
+    </>
+  );
+}
+
+export default App;
