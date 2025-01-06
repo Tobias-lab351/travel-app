879503b3ff0eefba57f05cd2e849db4db7036966
Tobias-lab351 <todhiambo87@gmail.com>
2025-01-06 14:05:04 +0300

    updated components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Destination from "./components/destination/Destination";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Search from "./components/Search/Search";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Destination />
+      <Search />
     </>
   );
 }
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,7 +3,7 @@ import "./Search.css"
 export default function Search() {
   return (
     <div>
-      
+      <h1>search destination</h1>
     </div>
   )
 }
